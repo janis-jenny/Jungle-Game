5b49fc289e55afda086901637a6ffd87a737b7af
janis-jenny <pcardenasvm@gmail.com>
2021-06-15 16:58:21 -0500

    add last platform to end the game and rotate the coins within its frames

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -7,19 +7,18 @@ import platform2 from '../assets/platform2.png'
 import platform3 from '../assets/platform3.png'
 import coin from '../assets/Coin.png'
 
-
 const gameState = { 
   score: 0,
   speed: 240,
   ups: 380,
-  width: 2320,
+  width: 2332,
   height: 585,
  };
 
 export default class MainScene extends Phaser.Scene {
   constructor() {
     super({ key: 'MainScene' });
-    this.heights = [null, 5, 2, null, 4, 3, 5, 2, 8, 4, 3];
+    this.heights = [null, 5, 2, 6, 4, 3, 5, 2, 8, 6];
     this.levelKey = MainScene
   }
 
@@ -29,7 +28,7 @@ export default class MainScene extends Phaser.Scene {
     this.load.image('platform2', platform2);
     this.load.image('platform3', platform3);
     this.load.spritesheet('gamora_walk', gamoraWalk, { frameWidth: 30, frameHeight: 36 });
-    this.load.spritesheet('coin', coin, { frameWidth: 9, frameHeight: 10 });
+    this.load.spritesheet('coin', coin, { frameWidth: 9.5, frameHeight: 10 });
   }
   // 
   create() {
@@ -42,7 +41,7 @@ export default class MainScene extends Phaser.Scene {
 
     const plat1Positions = [
       { x: 340, y: 585 }, { x: 580, y: 585 }, { x: 1010, y: 585 }, { x: 1250, y:  585 },
-      { x: 1660, y: 585 }, { x: 1900, y:  585 },
+      { x: 1660, y: 585 }, { x: 1900, y:  585 }, { x: 2200, y: 330 },
     ];
     plat1Positions.forEach(plat => {
       platforms.create(plat.x, plat.y, 'bigplatform1').setScale(.4).refreshBody();
@@ -84,13 +83,13 @@ export default class MainScene extends Phaser.Scene {
 
     const coins = this.physics.add.group({
       key: 'coin',
-      repeat: 10,
-      setXY: { x: 300, y: 0, stepX: 70 }
-    }); 
-  
+      repeat: 22,
+      setXY: { x: 300, y: 0, stepX: 80 }
+    });
+
     coins.children.iterate(function (child) {
       child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8)).setScale(2.3);
-     
+      child.anims.play('rotate')
     });
    
     // Displays initial Score: 0 text
@@ -115,7 +114,7 @@ export default class MainScene extends Phaser.Scene {
     // gameState.platforms = this.physics.add.staticGroup();
     // If either is not a number it won't make a platform
       if (typeof yIndex === 'number' && typeof xIndex === 'number') {
-        gameState.platforms.create((220 * xIndex),  yIndex * 70, 'platform2').setOrigin(0, 0.5).setScale(.3).refreshBody();
+        gameState.platforms.create((205 * xIndex),  yIndex * 70, 'platform2').setOrigin(0, 0.5).setScale(.3).refreshBody();
       } 
   }
 
@@ -139,8 +138,8 @@ export default class MainScene extends Phaser.Scene {
       repeat: -1
     });
     // setting coin animation
-   /*  this.anims.create({
-      key: 'coin',
+    this.anims.create({
+      key: 'rotate',
       frames: this.anims.generateFrameNumbers('coin', {
           start: 0,
           end: 3
@@ -148,7 +147,7 @@ export default class MainScene extends Phaser.Scene {
       frameRate: 15,
       yoyo: true,
       repeat: -1
-    }); */
+    });
   }
   
   levelSetup() {
@@ -189,8 +188,8 @@ export default class MainScene extends Phaser.Scene {
         });
       }
     }
-
   }
+
   jump() {
     if (gameState.player.body.touching.down || this.jumps < 2) {
       gameState.player.setVelocityY(sceneoptions.jumpForce * -1);
